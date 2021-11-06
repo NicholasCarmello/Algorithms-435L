1c7968ffb1a2ea24ba1785a720028ed52a0e4f43
nicholascarmello <nick.carmello@yahoo.com>
2021-11-06 00:28:15 -0700

    added latex doc

--- assignment3/Searching.java
+++ assignment3/Searching.java
@@ -26,62 +26,61 @@ public class Searching {
             e.printStackTrace();
         }
 
-        
         String RandomItems[] = new String[42];
         Random rand = new Random();
 
-        //Randomly chooses 42 of the 666 items
+        // Randomly chooses 42 of the 666 items
         for (int i = 0; i < RandomItems.length; i++) {
             RandomItems[i] = myArray[rand.nextInt(666)];
-            
+
         }
 
-        //calls for each of the searches and the hash table.
+        // calls for each of the searches and the hash table.
         linearSearch(RandomItems, myArray);
         binarySearch(RandomItems, myArray);
         hashTable(RandomItems, myArray);
 
     }
 
-    
     public static void linearSearch(String array[], String searchArray[]) {
         int averageTimeForLinearSearch = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < searchArray.length; j++) {
-                //This compares each element in the magic items text file to the current random item
+                // This compares each element in the magic items text file to the current random
+                // item
                 if (array[i] == searchArray[j]) {
                     break;
                 }
-                //global count
+                // global count
                 averageTimeForLinearSearch += 1;
             }
         }
-        System.out.println("linear search is " +averageTimeForLinearSearch / 42);
+        System.out.println("linear search is " + averageTimeForLinearSearch / 42);
     }
-
 
     public static void binarySearch(String array[], String searchArray[]) {
         float averageTimeForBinarySearch = 0;
-       
 
         for (int i = 0; i < array.length; i++) {
-            
+
             int start = 0;
             int end = array.length - 1;
-            
+
             while (start <= end) {
 
                 int medium = start + (end - start) / 2;
-                
-                //if the item is equal to the current mid then it will break out of the while loop
+
+                // if the item is equal to the current mid then it will break out of the while
+                // loop
                 if (searchArray[medium].replaceAll("\\s", "").compareTo(array[i].replaceAll("\\s", "")) == 0) {
                     averageTimeForBinarySearch += 1;
                     break;
                 }
-                //if the item is less than the current mid then the start becomes the medium plus 1
+                // if the item is less than the current mid then the start becomes the medium
+                // plus 1
                 if (searchArray[medium].replaceAll("\\s", "").compareTo(array[i].replaceAll("\\s", "")) < 0) {
                     start = medium + 1;
-                //if the item is greater than the current mid then end becomes the medium - 1
+                    // if the item is greater than the current mid then end becomes the medium - 1
                 } else {
                     end = medium - 1;
 
@@ -90,32 +89,31 @@ public class Searching {
             }
 
         }
-        System.out.println("Binary search is " + averageTimeForBinarySearch /42);
+        System.out.println("Binary search is " + averageTimeForBinarySearch / 42);
     }
 
-    //This function makes a hash table
+    // This function makes a hash table
     public static void hashTable(String randomItems[], String array[]) {
-        //create a new array of nodes size 250 and initializing the indices to null
+        // create a new array of nodes size 250 and initializing the indices to null
         Node newArray[] = new Node[250];
-        for (int i = 0; i < newArray.length; i++){
+        for (int i = 0; i < newArray.length; i++) {
             newArray[i] = null;
         }
 
         for (int i = 0; i < array.length; i++) {
             Node newNode = new Node();
             int getHashCode = makeHashCode(array[i]);
-            //This checks to see if there isn't an item in the indice and then adds the first node if there isn't
+            // This checks to see if there isn't an item in the indice and then adds the
+            // first node if there isn't
             if (newArray[getHashCode] == null) {
-
                 newNode.name = array[i];
                 newArray[getHashCode] = newNode;
-
-
             } else {
-                //This will find the last position in the linked list and will add the new node to the end.
+                // This will find the last position
+                // in the linked list and will add the new node to the end.
                 newNode.name = array[i];
                 Node current = newArray[getHashCode];
-                
+
                 while (current.name != null) {
                     if (current.next == null) {
                         current.next = newNode;
@@ -125,34 +123,36 @@ public class Searching {
                 }
             }
         }
-    
-        
-        
         hashTableLookup(randomItems, newArray);
     }
-    
+
     public static void hashTableLookup(String[] randomitems, Node[] array) {
         float averageLookUpTime = 0;
         for (int i = 0; i < randomitems.length; i++) {
             int hashCode = makeHashCode(randomitems[i]);
             Node current = array[hashCode];
-            //this checks if the current node or the first node in the linked list is the item.
+            // this checks if the current node or the first node
+            // in the linked list is the
+            // item.
             if (current.name == randomitems[i]) {
                 averageLookUpTime += 1;
                 continue;
             } else {
-                //This goes through every node in the lniked list until it finds the correct item.
+                // This goes through every node in the
+                // linked list until it finds the correct
+                // item.
                 while (current.next != null) {
                     averageLookUpTime += 1;
                     if (current.name == randomitems[i]) {
                         break;
                     }
-                    current=current.next;
+                    current = current.next;
                 }
             }
 
         }
-        //This prints out of the total divided by the number of random items in the array.
+        // This prints out of the total divided by the number of random items in the
+        // array.
         System.out.println("Hash look up average is " + averageLookUpTime / 42);
     }
 
@@ -203,7 +203,7 @@ public class Searching {
     }
 }
 
-//Node class
+// Node class
 class Node {
     String name;
     Node next;
