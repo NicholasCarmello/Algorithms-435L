990c1a5d5c8514180ed2f07d4b3ff6db361c424e
nicholascarmello <nick.carmello@yahoo.com>
2021-12-07 12:28:59 -0800

    Almost finished sssp And bellman

--- assignment5/graph.java
+++ assignment5/graph.java
@@ -1,16 +1,48 @@
 import java.util.ArrayList;
+
 public class graph {
     ArrayList<graphNode> vertices = new ArrayList<graphNode>();
     ArrayList<graphNode> edges = new ArrayList<graphNode>();
 
-    public static void bellman() {
+    public static void bellman(graph G, graphNode source) {
+        int distanceAttribute[] = new int[G.vertices.size()];
+        boolean newBool = true;
+        singleSource(G, source, distanceAttribute);
 
-    }
-    public static void singleSource(){
+        for (int i = 1; i < G.vertices.size(); i++) {
+            for (int j = 0; j < G.edges.size(); j++) {
+                relax(G.edges.get(j).weight, G.edges.get(j).from, G.edges.get(j).to, distanceAttribute);
+            }
+        }
+        for (int j = 0; j < G.edges.size(); j++) {
+            if (distanceAttribute[G.edges.get(j).to -1 ] > (distanceAttribute[G.edges.get(j).from - 1] + G.edges.get(j).weight)){
+                newBool = false;
+          }
 
-    }
-    public static void relax(){
+        }
+        if (newBool){
+            System.out.println("\n" + "New Graph " );
+            for(int i = 0;i <G.vertices.size(); i ++){
+                System.out.println(1 + " -> " + (i + 1) + " cost is " + distanceAttribute[i]);
+            }
+        } 
+        
 
     }
     
+    public static void singleSource(graph newGraph, graphNode source, int distanceAttribute[]) {
+        for (int i = 0; i < newGraph.vertices.size(); ++i) {
+            distanceAttribute[i] = 1000;
+
+        }
+        distanceAttribute[0] = 0;
+    }
+
+    public static void relax(int weight, int from, int to, int[] distanceAttribute) {
+
+        if (distanceAttribute[from - 1] != 1000 && distanceAttribute[from - 1] + weight < distanceAttribute[to - 1]) {
+            distanceAttribute[to - 1] = distanceAttribute[from - 1] + weight;
+        }
+    }
+
 }
--- assignment5/ssspAndKnapsack.java
+++ assignment5/ssspAndKnapsack.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap; // import the HashMap class
-import java.util.Arrays;
 import java.util.Collections;
 
 public class ssspAndKnapsack {
     public static int newGraphCount = 0;
 
     public static void main(String[] args) {
-        String[] txtFileGraph = new String[666];
+        String[] txtFileGraph = new String[88];
         String[] spiceArray = new String[14];
         readFile("graphs2.txt", txtFileGraph);
         readFile("spice.txt", spiceArray);
 
         spiceAlgorithm(spiceArray);
+        createGraph(txtFileGraph);
     }
 
     public static void spiceAlgorithm(String[] spiceArray) {
         ArrayList<Integer> capacities = new ArrayList<Integer>();
-        HashMap<Integer,  spice> valueToName = new HashMap<Integer, spice>();
+        HashMap<Integer, spice> valueToName = new HashMap<Integer, spice>();
         ArrayList<Integer> values = new ArrayList<Integer>();
 
         for (int i = 0; i < spiceArray.length; i++) {
@@ -30,60 +30,54 @@ public class ssspAndKnapsack {
                 String[] nameLine = firstSplit[0].split(" ");
                 String[] priceLine = firstSplit[1].split(" ");
                 String[] quantityLine = firstSplit[2].split(" ");
-                spice newSpice = new spice(); 
+                spice newSpice = new spice();
 
-                int price = (int)Float.parseFloat(priceLine[priceLine.length-1]);
-                int quantity = Integer.parseInt(quantityLine[quantityLine.length-1]);
-                String name = nameLine[nameLine.length-1];
+                int price = (int) Float.parseFloat(priceLine[priceLine.length - 1]);
+                int quantity = Integer.parseInt(quantityLine[quantityLine.length - 1]);
+                String name = nameLine[nameLine.length - 1];
 
                 newSpice.total_price = price;
                 newSpice.qty = quantity;
                 newSpice.name = name;
-                
-                values.add(price/quantity);
+
+                values.add(price / quantity);
                 valueToName.put(price / quantity, newSpice);
             }
             if (lineSplit[0].equals("knapsack")) {
-                 String [] firstSplit = spiceArray[i].split(";");
-                 String [] getCapacity = firstSplit[0].split(" ");
-                
+                String[] firstSplit = spiceArray[i].split(";");
+                String[] getCapacity = firstSplit[0].split(" ");
+
                 capacities.add(Integer.parseInt(getCapacity[getCapacity.length - 1]));
             }
         }
 
-        Collections.sort(values,Collections.reverseOrder());
-        
-    
-        for(int i = 0; i < capacities.size(); i ++){
+        Collections.sort(values, Collections.reverseOrder());
 
+        for (int i = 0; i < capacities.size(); i++) {
 
             int runningTotal = 0;
             int currentCapacity = capacities.get(i);
-            for ( int k = 0; k < values.size(); k++){
+            for (int k = 0; k < values.size(); k++) {
                 int currentValue = values.get(k);
-                if (currentCapacity ==0){
+                if (currentCapacity == 0) {
                     break;
-                }
-                else{
-                    if (currentCapacity >= valueToName.get(currentValue).qty){
-                        
+                } else {
+                    if (currentCapacity >= valueToName.get(currentValue).qty) {
+
                         runningTotal += valueToName.get(currentValue).total_price;
                         currentCapacity -= valueToName.get(currentValue).qty;
-                    }
-                    else{
-                        float man = (float) currentCapacity/valueToName.get(currentValue).qty;
-                        
-                        
-                        float newVal = valueToName.get(currentValue).total_price*man;
+                    } else {
+                        float man = (float) currentCapacity / valueToName.get(currentValue).qty;
+
+                        float newVal = valueToName.get(currentValue).total_price * man;
                         runningTotal += (int) newVal;
-                        currentCapacity =0;
+                        currentCapacity = 0;
                     }
                 }
             }
-            System.out.println(runningTotal);
+            System.out.println("Knapsack of capacity " + capacities.get(i) + " is worth " + runningTotal);
         }
-        
-        
+
     }
 
     public static void createGraph(String[] graphArrayTxtFile) {
@@ -95,34 +89,47 @@ public class ssspAndKnapsack {
                     newGraphCount += 1;
                     continue;
                 }
-                for (int m = 0; m < newGraph.vertices.size(); m++) {
-
-                }
-                newGraph.bellman();
+                
+                newGraph.bellman(newGraph, newGraph.vertices.get(0));
+                
+                
                 newGraph.vertices.clear();
+                newGraph.edges.clear();
+                
 
             } else {
+
+                if (graphArrayTxtFile[i].equals("") || graphArrayTxtFile[i].equals(" ")) {
+                    continue;
+                }
                 String findArray[] = graphArrayTxtFile[i].split(" ");
 
                 if (findArray[1].equals("vertex")) {
+
                     graphNode newGraphNode = new graphNode();
                     newGraphNode.from = Integer.parseInt(findArray[2]);
                     newGraph.vertices.add(newGraphNode);
-                } else {
 
-                    for (int j = 0; j < newGraph.vertices.size(); j++) {
-                        if (newGraph.vertices.get(i).from == Integer.parseInt(findArray[2])) {
+                } else if (findArray[1].equals("edge")) {
+                    
+                            graphNode newGraphNode = new graphNode();
+                            newGraphNode.to = Integer.parseInt(findArray[4]);
+                            newGraphNode.from = Integer.parseInt(findArray[2]);
 
-                            newGraph.vertices.get(i).to = Integer.parseInt(findArray[4]);
-                            newGraph.vertices.get(i).weight = Integer.parseInt(findArray[5]);
+                            if (findArray.length == 6) {
+                                newGraphNode.weight = Integer.parseInt(findArray[5]);
+                            } else {
+                                newGraphNode.weight = Integer.parseInt(findArray[6]);
+                            }
 
-                        }
-
-                    }
+                            newGraph.edges.add(newGraphNode);;
                 }
 
             }
+            
         }
+        newGraph.bellman(newGraph, newGraph.vertices.get(0));
+
     }
 
     public static void readFile(String fileName, String[] newArray) {
@@ -132,9 +139,7 @@ public class ssspAndKnapsack {
             Scanner myScanner = new Scanner(readFile);
             while (myScanner.hasNextLine()) {
                 String data = myScanner.nextLine();
-                if (data.equals(" ")) {
-                    continue;
-                }
+
                 newArray[count] = data.toLowerCase();
                 count++;
             }
